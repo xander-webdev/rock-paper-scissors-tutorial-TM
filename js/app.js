35ec632a29442bab5ba68c30cfc03cf46e6bb5b7
xander-webdev <xwernecke@gmail.com>
2020-05-08 10:14:38 +0000

    added simple html, css sheets and busy working on the JS to make the game functional. This has been a great tutorial so far and it's solidifying my knowledge in JS as well as teaching me a few new things

---
+++ js/app.js
@@ -0,0 +1,59 @@
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
+
+//Play Game
+function play(e) {
+    restart.style.display = "inline-block";
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+
+    console.log(playerChoice,computerChoice, winner)
+
+}
+//Get Computers Choice
+
+function getComputerChoice() {
+    const rand = Math.random();
+    if (rand < 0.34) {
+        return 'rock';
+    } else if (rand <= 0.67) {
+        return 'paper';
+    } else {
+        return 'scissors'
+    }
+
+}
+// Get Game Winner
+function getWinner(p, c) {
+    if (p === c) {
+        return 'draw'
+    } else if (p === 'rock') {
+        if (c === 'paper') {
+            return 'computer'
+        } else {
+            return 'player'
+        }
+    } else if (p === 'scissors') {
+        if (c === 'rock') {
+            return 'computer'
+        } else {
+            return 'player'
+        }
+    } else if (p === 'paper') {
+        if (c === 'scissors') {
+            return 'computer'
+        } else {
+            return 'player'
+        }
+    }
+}
+//Event Listeners
+choices.forEach(choice => choice.addEventListener('click', play));
